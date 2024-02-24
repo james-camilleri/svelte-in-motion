f5f641f083f5e69d64d3fdfcf46ae65054cc56c9
James Camilleri <james@james.mt>
2024-02-24 21:32:59 +0000

    Set up reveal.js

--- projects/presentation/src/routes/+layout.svelte
+++ projects/presentation/src/routes/+layout.svelte
@@ -1,8 +1,27 @@
 <script lang="ts">
+  import './global.css'
+  import 'reveal.js/dist/reveal.css'
+  // import 'reveal.js/dist/theme/night.css'
+
   import { browser } from '$app/environment'
+  import Background from '$lib/components/layout/Background.svelte'
   import { initialiseReveal } from '$lib/utils'
 
   if (browser) {
     initialiseReveal()
   }
 </script>
+
+<div class="reveal">
+  <div class="slides">
+    <slot />
+  </div>
+</div>
+<Background />
+
+<style lang="css">
+  .reveal {
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
