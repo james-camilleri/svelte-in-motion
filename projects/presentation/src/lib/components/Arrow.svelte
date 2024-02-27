7d9221d110ef51659c1b0e6d6db6606d0be5368a
James Camilleri <james@james.mt>
2024-02-27 00:09:09 +0000

    Add arrow bullet

---
+++ projects/presentation/src/lib/components/Arrow.svelte
@@ -0,0 +1,24 @@
+<span>→</span>
+
+<style lang="scss">
+  span {
+    position: relative;
+    display: inline-block;
+    padding-inline-end: 0.5rem;
+    font-size: 1.5em;
+    font-variation-settings:
+      'wght' 1000,
+      'wdth' 75;
+    line-height: 0;
+    color: var(--yellow-orange);
+
+    &::before {
+      position: absolute;
+      top: 4px;
+      left: 3px;
+      z-index: -1;
+      color: var(--dark);
+      content: '→';
+    }
+  }
+</style>
